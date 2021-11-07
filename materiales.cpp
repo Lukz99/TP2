7008fa3a1d586f671e6185212d4c571a6a74ec25
Lucas <lledesma@fi.uba.ar>
2021-11-06 23:21:26 -0300

    cpp faltante

---
+++ materiales.cpp
@@ -0,0 +1,12 @@
+#include "materiales.h"
+
+using namespace std;
+
+Material::Material() {
+    this-> nombre_material = nombre_material;
+    this-> cantidad_material = cantidad_material;
+}
+
+Material::~Material() {
+
+}
